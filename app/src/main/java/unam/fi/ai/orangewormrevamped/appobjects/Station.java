e8d978ba75b269ad05cee4ecfa55c45159860bdb
GustavoVaAsc <quickgohan98@gmail.com>
2025-05-17 18:24:27 -0600

    I add the CSV info for lines 7, 8, 9 and some stations of Line A

--- app/src/main/java/unam/fi/ai/orangewormrevamped/appobjects/Station.java
+++ app/src/main/java/unam/fi/ai/orangewormrevamped/appobjects/Station.java
@@ -5,10 +5,10 @@ public class Station {
     private String name;
     // TODO: Add logo image
     private String logo_file;  // resource ID of the drawable
-    private HashSet<Character> lines;
+    private HashSet<String> lines;
     private double latitude;
     private double longitude;
-    public Station(int id, String name, String logo_file, HashSet<Character> lines, double latitude, double longitude){
+    public Station(int id, String name, String logo_file, HashSet<String> lines, double latitude, double longitude){
         this.id = id;
         this.name = name;
         this.logo_file = logo_file;
