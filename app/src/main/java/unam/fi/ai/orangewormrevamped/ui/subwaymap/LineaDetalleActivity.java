41587bf8d8987901edde0ffc84ffac20ba185909
Elizabeth Portilla <elysph9@gmail.com>
2025-05-20 18:49:38 -0600

    Menu Metro

--- app/src/main/java/unam/fi/ai/orangewormrevamped/ui/subwaymap/LineaDetalleActivity.java
+++ app/src/main/java/unam/fi/ai/orangewormrevamped/ui/subwaymap/LineaDetalleActivity.java
@@ -3,7 +3,6 @@ package unam.fi.ai.orangewormrevamped.ui.subwaymap;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,18 +15,19 @@ public class LineaDetalleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_linea_detalle);
 
         ImageView imagenLinea = findViewById(R.id.imagenLinea);
-        TextView tituloLinea = findViewById(R.id.tituloLinea);
+        //TextView tituloLinea = findViewById(R.id.tituloLinea);
 
         Intent intent = getIntent();
-        int numeroLinea = intent.getIntExtra("linea", 0); // ← Clave corregida
+        String lineaId = intent.getStringExtra("linea_id");
 
-        if (numeroLinea <= 0) {
-            tituloLinea.setText("Mapa General del Metro");
+        if (lineaId == null || lineaId.isEmpty()) {
+            //tituloLinea.setText("Mapa General del Metro");
             imagenLinea.setImageResource(R.drawable.mapa_general_metro);
         } else {
-            tituloLinea.setText("Línea " + numeroLinea);
+            //tituloLinea.setText("Línea " + lineaId);
+
             int resourceId = getResources().getIdentifier(
-                    "linea_" + numeroLinea,
+                    "linea_" + lineaId.toLowerCase(),
                     "drawable",
                     getPackageName()
             );
